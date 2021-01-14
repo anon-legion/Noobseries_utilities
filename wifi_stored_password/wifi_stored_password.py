745f504105318bf098e1b9e7386ee14cf39d02fb
=GV= <77139649+anon-legion@users.noreply.github.com>
2021-01-14 22:51:46 +0800

    first commit for Noobseries_utilities, add wifi_stored_password sourcecode and executable

---
+++ wifi_stored_password/wifi_stored_password.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 14:58:23 2020
+
+@author: Anon
+"""
+
+from datetime import datetime as dt
+import subprocess as sp
+import webbrowser as wb
+
+try:
+    wifis = [(ssid.split(':')[1][1:-1], password.strip().split(':')[1][1:]) for ssid in sp.check_output(['netsh','wlan', 'show', 'profiles']).decode('utf-8').split('\n') if 'All User Profile' in ssid for password in sp.check_output(['netsh', 'wlan', 'show', 'profile', ssid.split(':')[1][1:-1], 'key=clear']).decode('utf-8').split('\n') if 'Key Content' in password]
+except IndexError:
+    ssids = [line.split(':')[1][1:-1] for line in sp.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n') if 'All User Profile' in line]
+    wifis = []
+    for ssid in ssids:
+        try:
+            pw = [line.strip().split(':')[1][1:] for line in sp.check_output(['netsh', 'wlan', 'show', 'profile', ssid, 'key=clear']).decode('utf-8').split('\n') if 'Key Content' in line]
+            wifis.append((ssid, pw[0]))
+        except IndexError:
+            wifis.append((ssid, ''))
+date_tag = dt.strftime(dt.date(dt.now()), '%Y%m%d')
+with open(f'WIFI_stored_{date_tag}.txt', 'w') as f:
+    f.write('disclaimer: I am not responsible for how this program is used\n')
+    for wifi in wifis:
+        f.write(f'\nSSID: \t{wifi[0]}\n')
+        f.write(f'PW: \t{wifi[1]}\n')
+    f.write("\nEPSTEIN DIDN'T KILL HIMSELF!!!")
+    f.write('\n\n\nthanks!! \nby: =GV=')
+
+wb.open(f'WIFI_stored_{date_tag}.txt')
